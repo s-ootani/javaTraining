49df458be0e7d9e1d075ad5f68d199b1ca433045
ootani <EXCITE-14304@EXCITE-14304-PC>
2015-03-10 18:08:10 +0900

    18章 演習問題3 wait(),notify()で排他制御を実装

--- src/FileIO.java
+++ src/FileIO.java
@@ -2,36 +2,36 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 
+public class FileIO extends Thread {
 
-public class FileIO extends Thread{
-
+	private static Object lock = new Object();
 	private BufferedReader br;
 	private BufferedWriter bw;
 
-	public FileIO(BufferedReader br, BufferedWriter bw){
+	public FileIO(BufferedReader br, BufferedWriter bw) {
 		this.br = br;
 		this.bw = bw;
 	}
 
-	public void run(){
-		String str;
-		do {
-			str = fileio();
-		} while (str != null);
-			str = fileio();
-	}
-
-	public synchronized String fileio(){
-		String str = null;
-		try {
-			str = br.readLine();
-			if(str != null){
-				bw.write(str);
+	public void run() {
+		while (true) {
+			synchronized (lock) {
+				try {
+					lock.notify();
+					String str = br.readLine();
+					if (str == null) {
+						break;
+					}
+					bw.write(str);
+					lock.wait();
+				} catch (IOException e) {
+					// TODO 自動生成された catch ブロック
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO 自動生成された catch ブロック
+					e.printStackTrace();
+				}
 			}
-		} catch (IOException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
 		}
-		return str;
 	}
 }
